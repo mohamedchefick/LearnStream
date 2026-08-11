79a5038a79aa9acd82b6828231d3866321503422
DavidAnato <david1anato@gmail.com>
2026-08-11 02:53:46 +0100

    chore: sync latest local changes to GitHub

--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -3,30 +3,25 @@ import Header from '../Helper/Header/index.vue';
 import Hero from './hero.vue';
 import Plateforme from './plateforme.vue';
 import Categories from './Categories.vue';
-import Temoignage from './temoignage.vue';
-import Decouvrir from './decouvrir.vue';
+// import Temoignage from './temoignage.vue';
+// import Decouvrir from './decouvrir.vue';
 import OurAchievements from './OurAchievements.vue';
-import Rejoin from './rejoigne.vue'
-import Footer from '../Helper/Footer/index.vue'
+import Rejoin from './rejoigne.vue';
+import Footer from '../Helper/Footer/index.vue';
 </script>
 
 <template>
     <div class="">
         <Header/>
         <Hero/>
-        <!--  -->
+        <!-- Section de découverte commentée -->
         <!-- <Decouvrir/> -->
-        <!--  -->
         <Plateforme />
-        <!--  -->
         <Categories />
-        <!--  -->
         <OurAchievements/>
-        <!--  -->
+        <!-- Section de témoignage commentée -->
         <!-- <Temoignage /> -->
-        <!--  -->
         <Rejoin/>
-        <!--  -->
         <Footer/>
     </div>
 </template>
